83f467705458ca237b797c22878ad0527bd386f5
Julien MARTIN <ju.martin@outlook.com>
2023-12-07 16:44:27 +0100

    add accueil and error component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {AccueilComponent} from "./accueil/accueil.component";
+import {ErrorComponent} from "./error/error.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AccueilComponent },
+  { path: 'not-found', component: ErrorComponent },
+  { path: '**', redirectTo: 'not-found'}
+];
---
+++ src/app/error/error.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterModule} from "@angular/router";
+
+@Component({
+  selector: 'app-error',
+  standalone: true,
+  imports: [
+    RouterModule
+  ],
+  templateUrl: './error.component.html',
+  styleUrl: './error.component.css'
+})
+export class ErrorComponent {
+
+}
