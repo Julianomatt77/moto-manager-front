4b2867287c0508bf3f91b0c3fabef078d0f7dc86
Julien MARTIN <ju.martin@outlook.com>
2024-02-02 11:10:51 +0100

    add metas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {SidebarComponent} from "./components/sidebar/sidebar.component";
+import {Meta} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,40 @@ import {SidebarComponent} from "./components/sidebar/sidebar.component";
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'moto-manager';
+  constructor(private metaService: Meta,) {
+    this.metaService.addTags([
+      {
+        name: 'author',
+        content: 'Julien MARTIN',
+      },
+      {
+        name: 'description',
+        content: 'Gérer les dépenses et entretiens réguliers de vos motos',
+      },
+      {
+        property: 'og:title',
+        content: 'Moto Manager',
+      },
+      {
+        property: 'og:site_name',
+        content: 'Moto Manager',
+      },
+      {
+        property: 'og:type',
+        content: 'website',
+      },
+      {
+        property: 'og:description',
+        content: 'Gérer les dépenses et entretiens réguliers de vos motos',
+      },
+      {
+        property: 'og:url',
+        content: 'https://moto-manager.martin-julien-dev.fr',
+      },
+      {
+        property: 'og:image',
+        content: 'assets/images/moto-manager.png',
+      },
+    ])
+  }
 }
