95a0df46c36f094943ee32cb6d7ba82babfcefc1
Julien MARTIN <ju.martin@outlook.com>
2024-05-30 10:31:53 +0200

    update meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, Inject} from '@angular/core';
+import {CommonModule, DOCUMENT} from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {SidebarComponent} from "./components/sidebar/sidebar.component";
 import {Meta} from "@angular/platform-browser";
@@ -12,15 +12,27 @@ import {Meta} from "@angular/platform-browser";
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  constructor(private metaService: Meta,) {
+  constructor(private metaService: Meta, @Inject(DOCUMENT) private document: Document) {
     this.metaService.addTags([
+      {
+        name: 'title',
+        content: 'Moto Manager',
+      },
       {
         name: 'author',
         content: 'Julien MARTIN',
       },
       {
         name: 'description',
-        content: 'Gérer les dépenses et entretiens réguliers de vos motos',
+        content: 'Moto Manager est votre nouvelle application vous permettant de suivre les dépenses et entretiens réguliers de vos motos',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1.0',
+      },
+      {
+        name: 'robots',
+        content: 'index, follow',
       },
       {
         property: 'og:title',
@@ -36,7 +48,7 @@ export class AppComponent {
       },
       {
         property: 'og:description',
-        content: 'Gérer les dépenses et entretiens réguliers de vos motos',
+        content: 'Moto Manager est votre nouvelle application vous permettant de suivre les dépenses et entretiens réguliers de vos motos',
       },
       {
         property: 'og:url',
@@ -44,8 +56,13 @@ export class AppComponent {
       },
       {
         property: 'og:image',
-        content: 'assets/images/moto-manager.png',
+        content: 'https://moto-manager.martin-julien-dev.fr/assets/images/moto-manager.png',
       },
     ])
   }
+
+  ngOnInit() {
+    this.document.documentElement.lang = 'fr';
+  }
+
 }
